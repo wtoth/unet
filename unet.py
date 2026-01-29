4c05d36f51eb49d24e1b94f7ec6c1177c0b8da4b
wtoth <wtoth@umich.edu>
2026-01-29 07:18:00 -0500

    update upsampling to use ConvTranspose2d

--- unet.py
+++ unet.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-class UNetModel(nn.Module):
+class UNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.contraction1 = nn.Sequential(
@@ -38,10 +38,10 @@ class UNetModel(nn.Module):
         self.max_pool = nn.MaxPool2d(2, stride=2)
         
         # Expansion Steps
-        self.up_conv1 = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
-        self.up_conv2 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
-        self.up_conv3 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
-        self.up_conv4 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.up_conv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up_conv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+        self.up_conv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up_conv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
 
         self.expansion1 = nn.Sequential(
             nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=3),
@@ -65,30 +65,40 @@ class UNetModel(nn.Module):
     def forward(self, x):
         # Contraction Steps
         contraction1_out = self.contraction1(x)
+        print("1", contraction1_out.shape)
         x = self.max_pool(contraction1_out)
+        print("1->2", x.shape)
         contraction2_out = self.contraction2(x)
+        print("2", contraction2_out.shape)
         x = self.max_pool(contraction2_out)
-        contraction3_out = self.contraction1(x)
+        print("2->3", x.shape)
+        contraction3_out = self.contraction3(x)
+        print("3", contraction3_out.shape)
         x = self.max_pool(contraction3_out)
+        print("3->4", x.shape)
         contraction4_out = self.contraction4(x)
+        print("4", contraction4_out.shape)
         x = self.max_pool(contraction4_out)
+        print("4->5", x.shape)
         contraction5_out = self.contraction5(x)
+        print("5", contraction5_out.shape)
 
         # Expansion Steps 
         x = self.up_conv1(contraction5_out) # up conv
-        x = x.concat(contraction4_out) # concat contraction val
+        print(x.shape, contraction4_out.shape)
+        x = torch.cat((x, contraction4_out)) # x.concat(contraction4_out) # concat contraction val
         x = self.expansion1(x) # Expand
 
         x = self.up_conv2(x)
-        x = x.concat(contraction3_out)
+        x = torch.cat((x, contraction3_out)) # x.concat(contraction3_out)
         x = self.expansion2(x)
 
         x = self.up_conv3(x)
-        x = x.concat(contraction2_out)
+        x = torch.cat((x, contraction2_out)) # x.concat(contraction2_out)
         x = self.expansion3(x)
 
         x = self.up_conv4(x)
-        x = x.concat(contraction1_out)
+        x = torch.cat((x, contraction1_out)) # x.concat(contraction1_out)
         x = self.expansion4(x)
         return x
 
